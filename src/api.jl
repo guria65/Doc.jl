1efac10576ada085e3f86fc60ac968c666e375da
Daniel Carrera <dcarrera@gmail.com>
2014-01-07 14:43:25 +0100

    First serious implementation of help() and apropos().

--- src/api.jl
+++ src/api.jl
@@ -19,6 +19,42 @@ DocDict  = Dict{String,DocEntry}
 
 DOC = DocDict()
 
-function help(key)
-	DOC[ string(key) ][:description]
+
+function apropos(s::Union(String,Regex))
+	f(key)  = typeof(s) == Regex ? ismatch(s,key) : contains(key,s)
+	matches = filter(f, keys(DOC))
+	
+	if length(matches) == 0
+		return "There are no matches for '$s'"
+	else
+		return "Matches for '$s':\n\n    " * join(matches,"\n    ")
+	end
 end
+
+help(m::String) = haskey(DOC,s) ? DOC[s] : apropos(s)
+
+# 
+# See also: https://github.com/JuliaLang/julia/blob/master/base/methodshow.jl
+#           xdump(Method)
+#           xdump(m.func.code)
+# 
+function help(m::Method)
+	fun = string(m.func.code.name)
+	sig = match(r"^(\(.+\)) at .*\:\d+", string(m).captures[1])
+	str = fun * sig # Ex: "foo" * "(x::Real)" == "foo(x::Real)"
+	
+	haskey(DOC,str) ? DOC[str] : help(fun)
+end
+
+function help(f::Function)
+	s = string(f)
+	
+	if haskey(DOC,s)
+		DOC[s]
+	else
+		# If we can identify a unique method, try to use that.
+		ms = collect( methods(f) )
+		length(ms) == 1 ? help(ms[1]) : apropos(s)
+	end
+end
+
