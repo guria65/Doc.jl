dbb759247367b5d80bd3f36039275bb0e0dde4d3
Daniel Carrera <dcarrera@gmail.com>
2014-01-07 10:04:52 +0100

    Merge macros.jl and parser.jl

--- src/asciidoc.jl
+++ src/asciidoc.jl
@@ -1,11 +1,24 @@
 doc"""
 :author: Daniel Carrera
-:date:   2014-01-06
+:date:   2014-01-07
 
-== Parser
+== Asciidoc implementation
 
 ...
 """
+
+export @doc, @doc_str, @doc_mstr
+
+macro doc_str(s)   init(s)   end
+macro doc_mstr(s)  init(s)   end
+
+macro doc(s,f)
+	str = string(typeof(s) == Expr ? eval(s) : s) # Docstring.
+	fun = string(typeof(f) == Expr ? eval(f) : f) # Function name.
+	
+	DOC[fun] = init(str)
+end
+
 
 #
 # Creates a DocEntry object and pulls out the metadata.
