b08809d83b6b3930f23b722d6bf2c3f8e3509849
Daniel Carrera <dcarrera@gmail.com>
2014-01-10 04:39:24 +0100

    Feature: Parse single-line Asciidoc headings.

--- src/lib/JuliaDoc.jl
+++ src/lib/JuliaDoc.jl
@@ -17,18 +17,40 @@ So far I have only implemented a basic data structure to hold
 the parsed documentation. The next step is to parse JuliaDoc
 into this data structure and then convert to other formats
 like HTML.
+
+NOTE:  I think that `Node` should have another field to store some
+       kind of metadata. For example, I'd like to have :section
+       tags with a :level field.
 """
 
+typealias Meta Dict{Symbol,String}
+
+#
+# TODO: Replace meta::Dict by meta::Meta -- don't know how.
+#
 type Node
 	tag::Symbol
-	contents::Array{Union(Node,String)}
-	Node(tag,cont) = new(tag, [cont])
+	content::Union(String,Array{Union(Node,String)})
+	meta::Dict
+	
+	Node(tag,content)      = new(tag,content,Dict())
+	Node(tag,content,meta) = new(tag,content,meta)
 end
 
-type JDoc
-	docstring::String
-	root::Node
-	JDoc(docstr) = new(docstr, Node(:root,docstr))
+typealias Item Union(Node,String)
+
+#
+# Empty lines have syntactic meaning. Be sure to keep them.
+#
+function lines(str)
+	split(str, "\n", true)
+end
+
+#
+# Convenience function.
+#
+function items(obj::Node)
+	typeof(obj.content) == String ? [obj.content] : obj.content
 end
 
 
@@ -37,12 +59,58 @@ end
 #
 function JuliaDoc(str::String)
 	
-	JDoc(str)
+	root = Node(:root, docstr)
 	
+	#
+	# Top-level headings.
+	#
+	parse_headings!(root)
+	
+	return root
 end
 
-
-
-
+function parse_headings!(obj::Node, level::Integer=1)
+	content = Item[] # New content.
+	
+	regex = Regex("^={$level}\s*([^=].*)\$")
+	
+	#
+	# When parsing headings, the content is always a single string.
+	#
+	for line in lines(obj.content)
+		if ismatch(regex,line)
+			meta = {
+				:level => level,
+				:title => strip(match(regex,line).captures[1])
+			}
+			push!(content, Node(:section,"",meta))
+		else
+			if length(content) == 0
+				push!(content, line)
+			else
+				if isa(content[end],String)
+					content[end] *= "\n" * line
+				else
+					# Last item must be section node.
+					content[end].content *= "\n" * line
+				end
+			end
+		end
+	end
+	
+	#
+	# Parse lower-level headings.
+	#
+	if level < 5
+		for item in content
+			if isa(item,Node)
+				parse_headings!(item, level+1)
+			end
+		end
+	end
+	
+	
+	obj.content = content
+end
 
 
