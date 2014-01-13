ad9400e34735f10fcc9f57de8ae1de5b3792ae83
Daniel Carrera <dcarrera@gmail.com>
2014-01-13 02:39:54 +0100

    Edit documentation.

--- src/lib/parser.jl
+++ src/lib/parser.jl
@@ -77,10 +77,6 @@ IMPORTANT: This is a paragraph.
   Comment line 2
   ////
 ----
-
-TIP: I have also implemented a basic tree data structure to hold the
-parsed documentation. The next step is to parse JuliaDoc into
-this data structure and then convert to other formats like HTML.
 "
 
 export parse_jdoc, JNode
