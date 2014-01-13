a528129bfff7d347c43b3cc6fb2ffe4a11de310d
Daniel Carrera <dcarrera@gmail.com>
2014-01-13 02:27:52 +0100

    Update block tests to handle paragraphs correctly.

--- test/jdoc_parse_blocks.jl
+++ test/jdoc_parse_blocks.jl
@@ -1,95 +1,3 @@
-
-docstr = """
-....
-Literal line 1
-Literal line 2
-....
-"""
-obj = parse_jdoc(docstr)
-
-@test obj.content[1].tag == :literal
-@test obj.content[1].meta[:group] == :verbatim
-@test obj.content[1].content[1] == "Literal line 1"
-@test obj.content[1].content[2] == "Literal line 2"
-
-docstr = """
-----
-Listing line 1
-Listing line 2
-----
-"""
-obj = parse_jdoc(docstr)
-
-@test obj.content[1].tag == :listing
-@test obj.content[1].meta[:group] == :verbatim
-@test obj.content[1].content[1] == "Listing line 1"
-@test obj.content[1].content[2] == "Listing line 2"
-
-docstr = """
-====
-Example line 1
-Example line 2
-====
-"""
-obj = parse_jdoc(docstr)
-
-@test obj.content[1].tag == :example
-@test obj.content[1].meta[:group] == :normal  
-@test obj.content[1].content[1] == "Example line 1"
-@test obj.content[1].content[2] == "Example line 2"
-
-docstr = """
-****
-Sidebar line 1
-Sidebar line 2
-****
-"""
-obj = parse_jdoc(docstr)
-
-@test obj.content[1].tag == :sidebar
-@test obj.content[1].meta[:group] == :normal  
-@test obj.content[1].content[1] == "Sidebar line 1"
-@test obj.content[1].content[2] == "Sidebar line 2"
-
-docstr = """
-____
-Verse line 1
-Verse line 2
-____
-"""
-obj = parse_jdoc(docstr)
-
-@test obj.content[1].tag == :passage  
-@test obj.content[1].meta[:group] == :normal  
-@test obj.content[1].content[1] == "Verse line 1"
-@test obj.content[1].content[2] == "Verse line 2"
-
-docstr = """
-|===
-Table line 1
-Table line 2
-|===
-"""
-obj = parse_jdoc(docstr)
-
-@test obj.content[1].tag == :table  
-@test obj.content[1].meta[:group] == :other   
-@test obj.content[1].content[1] == "Table line 1"
-@test obj.content[1].content[2] == "Table line 2"
-
-docstr = """
-++++
-Pass line 1
-Pass line 2
-++++
-"""
-obj = parse_jdoc(docstr)
-
-@test obj.content[1].tag == :pass   
-@test obj.content[1].meta[:group] == :other   
-@test obj.content[1].content[1] == "Pass line 1"
-@test obj.content[1].content[2] == "Pass line 2"
-
 docstr = """
 ////
 Comment line 1
@@ -100,3 +8,97 @@ obj = parse_jdoc(docstr)
 
 @test obj.tag == :root
 @test obj.content == []
+
+docstr = """
+|===
+Table line 1
+Table line 2
+|===
+"""
+obj = parse_jdoc(docstr).content[1]
+
+@test obj.tag == :table  
+@test obj.meta[:group] == :other   
+@test obj.content[1] == "Table line 1"
+@test obj.content[2] == "Table line 2"
+
+docstr = """
+....
+Literal line 1
+Literal line 2
+....
+"""
+obj = parse_jdoc(docstr).content[1]
+
+@test obj.tag == :literal
+@test obj.meta[:group] == :verbatim
+@test obj.content[1] == "Literal line 1"
+@test obj.content[2] == "Literal line 2"
+
+docstr = """
+----
+Listing line 1
+Listing line 2
+----
+"""
+obj = parse_jdoc(docstr).content[1]
+
+@test obj.tag == :listing
+@test obj.meta[:group] == :verbatim
+@test obj.content[1] == "Listing line 1"
+@test obj.content[2] == "Listing line 2"
+
+docstr = """
+++++
+Pass line 1
+Pass line 2
+++++
+"""
+obj = parse_jdoc(docstr).content[1]
+
+@test obj.tag == :pass   
+@test obj.meta[:group] == :other   
+@test obj.content[1] == "Pass line 1"
+@test obj.content[2] == "Pass line 2"
+
+docstr = """
+====
+Example line 1
+Example line 2
+====
+"""
+obj = parse_jdoc(docstr).content[1]
+
+@test obj.tag == :example
+@test obj.meta[:group] == :normal  
+@test obj.content[1].tag == :para
+@test obj.content[1].content[1] == "Example line 1"
+@test obj.content[1].content[2] == "Example line 2"
+
+docstr = """
+****
+Sidebar line 1
+Sidebar line 2
+****
+"""
+obj = parse_jdoc(docstr).content[1]
+
+@test obj.tag == :sidebar
+@test obj.meta[:group] == :normal  
+@test obj.content[1].tag == :para
+@test obj.content[1].content[1] == "Sidebar line 1"
+@test obj.content[1].content[2] == "Sidebar line 2"
+
+docstr = """
+____
+Verse line 1
+Verse line 2
+____
+"""
+obj = parse_jdoc(docstr).content[1]
+
+@test obj.tag == :passage  
+@test obj.meta[:group] == :normal  
+@test obj.content[1].tag == :para
+@test obj.content[1].content[1] == "Verse line 1"
+@test obj.content[1].content[2] == "Verse line 2"
