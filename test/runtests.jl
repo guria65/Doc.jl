da191a6cc074d3b04cba74161aa6d0175d731edd
Daniel Carrera <dcarrera@gmail.com>
2014-01-17 07:36:01 +0100

    Rename tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ tests = [ "parse_sections", "parse_paragraphs", "parse_blocks" ]
 for t in tests
     println("Running jdoc_$t ...")
     try
-        include("jdoc_$t.jl")
+        include("$t.jl")
     catch err
         println()
         rethrow(err)
